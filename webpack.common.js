a1ef7e4cc1f79d50ed30c26f96fd5257e6a925f7
James Walker <jamesmawalker@gmail.com>
2020-06-21 16:26:46 +0700

    Reconfigure config into dev, production, and common config files. Add new scripts for dev and build versions.

--- webpack.common.js
+++ webpack.common.js
@@ -2,8 +2,6 @@ const path = require("path");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: "development",
-  entry: "./src/index.js",
   output: {
     filename: "main.[contentHash].js",
     path: path.resolve(__dirname, "dist"),
